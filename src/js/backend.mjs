6b2893e389bfdf89a9a288fb781600d0bd6a7299
aymonier-elias <elias.aymonier@edu.univ-fcomte.fr>
2025-02-21 11:57:35 +0100

    Fin heure de tp

--- src/js/backend.mjs
+++ src/js/backend.mjs
@@ -14,16 +14,40 @@ export async function allMaison() {
         return [];
     }
 }
+export async function allAgent() {
+    try {
+        let data = await pb.collection('agent').getFullList({ sort: '-created' });
+        data = data.map((maison) => {
+            maison.imgURL = pb.files.getURL(maison, maison.images);
+            return maison;
+        })
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant la liste des mainsons', error);
+        return [];
+    }
+}
 
 export async function getOffre(id) {
     try {
-        let data = await pb.collection('maison').getOne(id);
+        let data = await pb.collection('Maison').getOne(id);
         data.imgURL = pb.files.getURL(data, data.images);
         return data;
     } catch (e) {
         console.error(e)
     }
 }
+
+export async function getAgent(id) {
+    try {
+        let data = await pb.collection('agent').getOne(id);
+        data.imgURL = pb.files.getURL(data, data.images);
+        return data;
+    } catch (e) {
+        console.error(e)
+    }
+}
+
 
 export async function bySurface(surfMin) {
     let records = await pb.collection('Maison').getFullList({ filter: `surface > '${surfMin}'` })
@@ -36,7 +60,7 @@ export async function bySurface(surfMin) {
 
 export async function addOffre(house) {
     try {
-        await pb.collection('maison').create(house);
+        await pb.collection('Maison').create(house);
         return {
             success: true,
             message: 'Offre ajoutée avec succès'
@@ -47,5 +71,36 @@ export async function addOffre(house) {
             success: false,
             message: 'Une erreur est survenue en ajoutant la maison'
         };
+    }
+}
+
+export async function filterByPrix(prixMin, prixMax) {
+    try {
+        let data = await pb.collection('Maison').getFullList({
+            sort: '-created',
+            filter: ` '${prixMin} < prix < ${prixMax}`
+        });
+        data = data.map((maison) => {
+            maison.imageUrl = pb.files.getURL(maison, maison.image);
+            return maison;
+        });
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en filtrant la liste des maisons', error);
+        return [];
+    }
+}
+
+export async function getOffreByAgent(id) {
+    try {
+        let data = await pb.collection('Maison').getFullList({filter: `agent = "${id}"`});
+        data = data.map((maison) => {
+            maison.imgURL = pb.files.getURL(maison, maison.images);
+            return maison;
+        })
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant la liste des mainsons', error);
+        return [];
     }
 }
